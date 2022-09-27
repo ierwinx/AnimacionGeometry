72e4650afb2abbdf470965cfb32f196275aad727
Eяw!и Gontier <ierwinx@me.com>
2022-09-26 22:32:38 -0500

    Initial Commit

---
+++ Geometry/GeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryApp.swift
+//  Geometry
+//
+//  Created by Erwin Luz León on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
